2bec13b89f51c278d5b438fddade92cac088e796
rguruz09 <raghavendraramakrishnaiah.guru@sjsu.edu>
2015-12-01 02:48:56 -0800

    Updatated login and signup controller

--- LostNFound_Client/www/js/controllers.js
+++ LostNFound_Client/www/js/controllers.js
@@ -2,33 +2,37 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
+
 .controller('NewlostItemCtrl', function($scope,$state,$http,$ionicLoading, UserSession) {
 
-/*
-google.maps.event.addDomListener(window, 'load', function() {
-        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+  console.log("listening..");
+  
+  google.maps.event.addDomListener(window, 'load', function() {
+     console.log("listening again..");
+     // var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
  
-        var mapOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+     //    var mapOptions = {
+     //        center: myLatlng,
+     //        zoom: 16,
+     //        mapTypeId: google.maps.MapTypeId.ROADMAP
+     //    };
  
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+     //    var map = new google.maps.Map(document.getElementById("dvMap"), mapOptions);
  
-        navigator.geolocation.getCurrentPosition(function(pos) {
-            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-            var myLocation = new google.maps.Marker({
-                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-                map: map,
-                title: "My Location"
-            });
-        });
+     //    navigator.geolocation.getCurrentPosition(function(pos) {
+     //        map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+     //        var myLocation = new google.maps.Marker({
+     //            position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+     //            map: map,
+     //            title: "My Location"
+     //        });
+     //    });
  
-        $scope.map = map;
-    });
+     //    $scope.map = map;     
+  });
+  
 
-*/
+
   $scope.data = {};
   $scope.submit = function() {
 
